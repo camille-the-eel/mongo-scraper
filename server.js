e5c4102a292f180197cc592fd206838da9b58389
Camille <c.hughes64@gmail.com>
2019-03-26 22:03:04 -0400

    scrape with summary

--- server.js
+++ server.js
@@ -17,15 +17,18 @@ app.use(express.static("public"));
 // mongoose.connect("mongodb://localhost/mongo-scraper", { useNewUrlParser: true });
 
 app.get("/scrape", function(req, res) {
-    axios.get("https://www.bbc.com/news/world").then(function(response) {
+    axios.get("https://www.bbc.co.uk/programmes/p0374bx8").then(function(response) {
         var $ = cheerio.load(response.data);
 
-        $("span.title-link__title-text").each(function(i, element) {
+        $("div.promotion").each(function(i, element) {
             var result = {};
 
-            result.title = $(this).text();
-            result.link = $(this).parent("h3").parent("a").attr("href");
-            console.log("TITLE", result.title, "LINK", result.link);
+            result.img = $(this).children("div.promotion__img").children("img").attr("src");
+
+            result.title = $(this).children("div.promotion__body").children("div.promotion__body__inner").children("h4.promotion__titles").children("span.promotion__title").children("a").text();
+            result.link = $(this).children("div.promotion__body").children("div.promotion__body__inner").children("h4.promotion__titles").children("span.promotion__title").children("a").attr("href");
+            result.summary = $(this).children("div.promotion__body").children("div.promotion__body__inner").children("p.promotion__synopsis").children("span").text();
+            console.log("IMG", result.img, "TITLE", result.title, "LINK", result.link, "SUMMARY", result.summary);
 
         });
     });
