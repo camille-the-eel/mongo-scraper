e3a63d63f8d8f8b7263d6504152583e4c936f316
Camille <c.hughes64@gmail.com>
2019-03-30 23:55:36 -0400

    r(app)

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,65 +1,65 @@
-// var db = require("../models");
-// var axios = require("axios");
-// var cheerio = require("cheerio");
+var db = require("../models");
+var axios = require("axios");
+var cheerio = require("cheerio");
 
-// module.exports = function (app) {
+module.exports = function (app) {
 
-//     app.get("/scrape", function(req, res) {
-//         axios.get("https://www.bbc.co.uk/programmes/p0374bx8").then(function(response) {
-//             var $ = cheerio.load(response.data);
+    app.get("/scrape", function(req, res) {
+        axios.get("https://www.bbc.co.uk/programmes/p0374bx8").then(function(response) {
+            var $ = cheerio.load(response.data);
     
-//             $("div.promotion").each(function(i, element) {
-//                 var result = {};
+            $("div.promotion").each(function(i, element) {
+                var result = {};
     
-//                 result.title = $(this).children("div.promotion__body").children("div.promotion__body__inner").children("h4.promotion__titles").children("span.promotion__title").children("a").text();
-//                 result.link = $(this).children("div.promotion__body").children("div.promotion__body__inner").children("h4.promotion__titles").children("span.promotion__title").children("a").attr("href");
-//                 result.summary = $(this).children("div.promotion__body").children("div.promotion__body__inner").children("p.promotion__synopsis").children("span").text();
-//                 // result.img = $(this).children("div.promotion__img").children("img").attr("src");
+                result.title = $(this).children("div.promotion__body").children("div.promotion__body__inner").children("h4.promotion__titles").children("span.promotion__title").children("a").text();
+                result.link = $(this).children("div.promotion__body").children("div.promotion__body__inner").children("h4.promotion__titles").children("span.promotion__title").children("a").attr("href");
+                result.summary = $(this).children("div.promotion__body").children("div.promotion__body__inner").children("p.promotion__synopsis").children("span").text();
+                // result.img = $(this).children("div.promotion__img").children("img").attr("src");
     
-//                 db.Article.create(result)
-//                     .then(function(dbArticle) {
-//                         console.log(dbArticle);
-//                     })
-//                     .catch(function(err) {
-//                         console.log(err);
-//                     });
-//             });
-//             res.send("Scrape Complete");
-//         });
-//     });
+                db.Article.create(result)
+                    .then(function(dbArticle) {
+                        console.log(dbArticle);
+                    })
+                    .catch(function(err) {
+                        console.log(err);
+                    });
+            });
+            res.send("Scrape Complete");
+        });
+    });
     
-//     app.get("/articles", function(req, res) {
-//         db.Article.find({})
-//             .then(function(dbArticle) {
-//                 res.json(dbArticle);
-//             })
-//             .catch(function(err) {
-//                 res.json(err);
-//             });
-//     });
+    app.get("/articles", function(req, res) {
+        db.Article.find({})
+            .then(function(dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function(err) {
+                res.json(err);
+            });
+    });
     
-//     app.get("/articles/:id", function(req,res) {
-//         db.Article.findOne({ _id: req.params.id })
-//             .populate("comment")
-//             .then(function(dbArticle) {
-//                 res.json(dbArticle);
-//             })
-//             .catch(function(err) {
-//                 res.json(err);
-//             });
-//     });
+    app.get("/articles/:id", function(req,res) {
+        db.Article.findOne({ _id: req.params.id })
+            .populate("comment")
+            .then(function(dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function(err) {
+                res.json(err);
+            });
+    });
     
-//     app.post("/articles/:id", function(req, res) {
-//         db.Comment.create(req.body)
-//             .then(function(dbComment) {
-//                 return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
-//             })
-//             .then(function(dbArticle) {
-//                 res.json(dbArticle);
-//             })
-//             .catch(function(err) {
-//                 res.json(err);
-//             });
-//     });
+    app.post("/articles/:id", function(req, res) {
+        db.Comment.create(req.body)
+            .then(function(dbComment) {
+                return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
+            })
+            .then(function(dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function(err) {
+                res.json(err);
+            });
+    });
 
-// };
+};
