1c8fba7b6a3e37953c113f9981e3e33be9d32510
Camille <c.hughes64@gmail.com>
2019-03-30 20:02:35 -0400

    routing

--- public/app.js
+++ public/app.js
@@ -1,1 +1,5 @@
-$.getJSON("")
+$.getJSON("/articles", function(data) {
+    for (var i = 0; i < data.length; i++) {
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    }
+})
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var axios = require("axios");
-var cheerio = require("cheerio");
 var path = require("path");
-
-var db = require("./models");
 
 var PORT = 3000;
 var app = express();
@@ -18,73 +14,12 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-//ROUTING EX
-// require("./routes/html-routes")(app);
+//ROUTING
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
 
 
 mongoose.connect("mongodb://localhost/mongo-scraper", { useNewUrlParser: true });
-
-app.get("/", function (req, res) {
-    res.render("index");
-})
-
-app.get("/scrape", function(req, res) {
-    axios.get("https://www.bbc.co.uk/programmes/p0374bx8").then(function(response) {
-        var $ = cheerio.load(response.data);
-
-        $("div.promotion").each(function(i, element) {
-            var result = {};
-
-            result.title = $(this).children("div.promotion__body").children("div.promotion__body__inner").children("h4.promotion__titles").children("span.promotion__title").children("a").text();
-            result.link = $(this).children("div.promotion__body").children("div.promotion__body__inner").children("h4.promotion__titles").children("span.promotion__title").children("a").attr("href");
-            result.summary = $(this).children("div.promotion__body").children("div.promotion__body__inner").children("p.promotion__synopsis").children("span").text();
-            // result.img = $(this).children("div.promotion__img").children("img").attr("src");
-
-            db.Article.create(result)
-                .then(function(dbArticle) {
-                    console.log(dbArticle);
-                })
-                .catch(function(err) {
-                    console.log(err);
-                });
-        });
-        res.send("Scrape Complete");
-    });
-});
-
-app.get("/articles", function(req, res) {
-    db.Article.find({})
-        .then(function(dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
-});
-
-app.get("/articles/:id", function(req,res) {
-    db.Article.findOne({ _id: req.params.id })
-        .populate("comment")
-        .then(function(dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
-});
-
-app.post("/articles/:id", function(req, res) {
-    db.Comment.create(req.body)
-        .then(function(dbComment) {
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
-        })
-        .then(function(dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
-});
 
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
